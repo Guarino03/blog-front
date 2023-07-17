8c3de615d1ac2a731a28ab066b36b4df0cfcd477
Felipe Alves <felipealvesguarino@gmail.com>
2023-07-17 20:37:10 -0300

    finalizando as funcionalidades

--- app.js
+++ app.js
@@ -3,15 +3,34 @@ const mongoose = require("mongoose");
 const blogRouter = require("./routes/BlogRoutes");
 
 const app = express();
+const PORT = 27017
 
 //middleware
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/blogs", blogRouter);
 
 //configure mongoose
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/CRUD",
+  "mongodb://127.0.0.1:27017",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
